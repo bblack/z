b9d3d54043835f9313198ed0715d511fb0e5a7bd
Brian Black <black@gatech.edu>
2023-07-15 15:01:39 -0400

    test "eat"

--- test/zork.js
+++ test/zork.js
@@ -34,7 +34,43 @@ describe('Z', () => {
             "\n" +
             ">"
           )
+        });
+    });
+
+    it('produces the expected output upon "eat"', () => {
+      var collectedOutput = '';
+
+      return fs.readFile('test/zork1.z5')
+        .then((buf) => {
+          var ab = buf.buffer;
+          var z = new Z({
+            inputs: ['eat'],
+            onLog: () => {},
+            onOutput: (s) => collectedOutput += s
+          });
+
+          z.loadGame(ab);
+
+          // TODO: do this the right way instead of waiting 1 sec
+          return new Promise((resolve) => { setTimeout(resolve, 1_000); });
         })
-    })
-  })
-})
+        .then(() => {
+          assert.equal(collectedOutput,
+            "ZORK I: The Great Underground Empire\n" +
+            "Copyright (c) 1981, 1982, 1983 Infocom, Inc. All rights reserved.\n" +
+            "ZORK is a registered trademark of Infocom, Inc.\n" +
+            "Revision 88 / Serial number 840726\n" +
+            "\n" +
+            "West of House\n" +
+            "You are standing in an open field west of a white house, with a boarded front door.\n" +
+            "There is a small mailbox here.\n" +
+            "\n" +
+            ">eat\n" +
+            "What do you want to eat?\n" +
+            "\n" +
+            ">"
+          )
+        });
+    });
+  });
+});
